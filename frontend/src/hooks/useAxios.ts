0c28512933804b57c93115e955b1b27afef83e9e
KimYoungSoo <zerowater4704@gmail.com>
2024-12-29 02:17:31 +0900

    API_BASE_URL修正

--- frontend/src/hooks/useAxios.ts
+++ frontend/src/hooks/useAxios.ts
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL =
-  import.meta.env.MODE === "development" && import.meta.env.VITE_API_URL;
+const API_BASE_URL = import.meta.env.VITE_API_URL;
 
 const axiosInstance = axios.create({
   baseURL: API_BASE_URL,

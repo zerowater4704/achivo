3e783737293992da3e52cf2496231e86f12b0151
KimYoungSoo <zerowater4704@gmail.com>
2024-12-29 16:08:58 +0900

    moduel修正

--- backend/src/types/global.d.ts
+++ backend/src/types/global.d.ts
@@ -2,3 +2,38 @@ declare module "bcrypt";
 declare module "passport-google-oauth20";
 declare module "cors";
 declare module "bcryptjs";
+declare module "passport-google-oauth" {
+  import passport from "passport";
+
+  export interface Profile {
+    id: string;
+    displayName: string;
+    name?: {
+      familyName: string;
+      givenName: string;
+    };
+    emails?: Array<{ value: string }>;
+    photos?: Array<{ value: string }>;
+    provider: string;
+    _raw: string;
+    _json: any;
+  }
+
+  export interface IOAuth2StrategyOption {
+    clientID: string;
+    clientSecret: string;
+    callbackURL: string;
+  }
+
+  export class OAuth2Strategy extends passport.Strategy {
+    constructor(
+      options: IOAuth2StrategyOption,
+      verify: (
+        accessToken: string,
+        refreshToken: string,
+        profile: Profile,
+        done: (err: any, user?: any) => void
+      ) => void
+    );
+  }
+}
